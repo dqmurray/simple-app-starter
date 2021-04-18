2e56215c307405d331dcc8e128e35f4ae9a05044
dqmurray <d.quinn.murray@gmail.com>
2021-04-18 17:27:08 -0400

    MVP (prints names, labels duplicates)

--- monolithstarter-svc/src/main/java/com/validity/monolithstarter/service/AlgorithmHandler.java
+++ monolithstarter-svc/src/main/java/com/validity/monolithstarter/service/AlgorithmHandler.java
@@ -3,9 +3,25 @@ package com.validity.monolithstarter.service;
 import org.springframework.stereotype.Service;
 import java.io.*;
 import org.apache.commons.csv.*;
+import java.util.*;
 
 public class AlgorithmHandler {
-    public boolean levenshteinDuplicate(CSVRecord record1, CSVRecord record2, int scoreRequirement){
-
+    public static boolean levenshteinDuplicate(CSVRecord record1, CSVRecord record2, int distanceMax){
+        Iterator<String> r1 = record1.iterator();
+        Iterator<String> r2 = record2.iterator();
+        String e1;
+        String e2;
+        int distance = 0;
+        while (r1.hasNext() && r2.hasNext()){
+            e1 = r1.next();
+            e2 = r2.next();
+            if (e1.equals(e2)){
+                distance = distance;
+            }
+            else {
+                distance++;
+            }
+        }
+        return distance <= distanceMax;
     }
 }
--- monolithstarter-svc/src/main/java/com/validity/monolithstarter/service/HelloService.java
+++ monolithstarter-svc/src/main/java/com/validity/monolithstarter/service/HelloService.java
@@ -1,10 +1,44 @@
 package com.validity.monolithstarter.service;
 
 import org.springframework.stereotype.Service;
+import java.io.*;
+import org.apache.commons.csv.*;
 
 @Service
 public class HelloService {
     public String getHelloMessage() {
-        return "Hello from the server!";
+		return parseCSV();
+        //return "Hello from the server!";
+    }
+
+    public String parseCSV() {
+        CSVParser parser;
+        String response = "";
+
+		try {
+            FileReader Data = new FileReader("../test-files/normal.csv");
+            parser = CSVParser.parse(Data, CSVFormat.DEFAULT.withFirstRecordAsHeader());
+            CSVRecord previous = null;
+
+            for (CSVRecord csvRecord : parser) {
+
+                response += csvRecord.get(1);
+
+                if (previous != null && AlgorithmHandler.levenshteinDuplicate(csvRecord, previous, 5)){
+                    response += " [D], ";
+                }
+                else {
+                    response += ", ";
+                }
+                previous = csvRecord;
+
+            }
+
+		}
+		catch (Exception e) {
+			System.out.print("Error " + e.toString());
+		}
+
+		return response;
     }
 }
