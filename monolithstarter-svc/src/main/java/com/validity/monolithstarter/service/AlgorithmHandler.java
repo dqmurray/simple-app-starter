1ab58aba850407ae75f7ff9adfe897b49ddda700
dqmurray <d.quinn.murray@gmail.com>
2021-04-18 16:41:34 -0400

    Setting up github

---
+++ monolithstarter-svc/src/main/java/com/validity/monolithstarter/service/AlgorithmHandler.java
@@ -0,0 +1,11 @@
+package com.validity.monolithstarter.service;
+
+import org.springframework.stereotype.Service;
+import java.io.*;
+import org.apache.commons.csv.*;
+
+public class AlgorithmHandler {
+    public boolean levenshteinDuplicate(CSVRecord record1, CSVRecord record2, int scoreRequirement){
+
+    }
+}
